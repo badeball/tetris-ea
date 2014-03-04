c8903d522e7b411574b26f3cbabd357b1bcc0f5c
Jonas Amundsen <jonasba@gmail.com>
2014-03-04 13:19:38 +0100

    Initial work on MPI.

--- code/src/tetris.c
+++ code/src/tetris.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <signal.h>
+#include <mpi.h>
+#include <unistd.h>
 
 #include "board.h"
 #include "feature.h"
@@ -17,6 +19,13 @@
 #include "threading.h"
 
 #define PRINT_V(string, ...) if (opt.verbose) { printf(string, ##__VA_ARGS__); }
+
+#define MASTER_RANK      0
+#define CALC_PERM_TAG    1
+#define FEAT_WEIGHTS_TAG 2
+#define FEAT_ENABLED_TAG 3
+#define RESULT_TAG       4
+#define SHUTDOWN_TAG     4
 
 struct options opt = {
     .n_features_enabled    = 0,
@@ -50,6 +59,10 @@ struct options opt = {
 char* program_name;
 
 int user_exit = 0;
+
+// Used for MPI.
+int zero = 0;
+int one  = 1;
 
 void print_help_text () {
     printf(
@@ -199,159 +212,297 @@ int main (int argc, char **argv) {
         }
     }
 
-    if (opt.crossover_points > opt.n_features_enabled) {
-        opt.crossover_points = opt.n_features_enabled;
-    }
+    int rank;
 
-    if (!log_directory_defined && !opt.no_log) {
-        printf("Missing log directory. Terminating.\n");
-        return 1;
-    }
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    FILE * f;
+    opt.seedp += rank * 7; // Mulitple of 7 gives the addition some moment.
 
-    char time_format[50];
-    char run_log_directory[200];
-    char run_log_data_file[261];
-    char run_log_solution_file[262];
+    if (rank == MASTER_RANK) {
+        int n_workers;
 
-    if (!opt.no_log) {
-        time_t timer;
-        struct tm* tm_info;
-        time(&timer);
-        tm_info = localtime(&timer);
+        MPI_Comm_size(MPI_COMM_WORLD, &n_workers);
 
-        strftime(time_format, 50, "%Y-%m-%d-%H%M%S", tm_info);
+        if (opt.crossover_points > opt.n_features_enabled) {
+            opt.crossover_points = opt.n_features_enabled;
+        }
 
-        if (snprintf(run_log_directory, 200, "%s/%s", opt.log_directory, time_format) >= 200) {
-            printf("Unable to allocate run log directory. Terminating.\n");
+        if (!log_directory_defined && !opt.no_log) {
+            printf("Missing log directory. Terminating.\n");
             return 1;
         }
 
-        mkdir(run_log_directory, 0755);
+        FILE * f;
 
-        if (snprintf(run_log_data_file, 261, "%s/%s", run_log_directory, "fitness.dat") >= 261) {
-            printf("Unable to allocate data log path. Terminating.\n");
-            return 1;
+        char time_format[50];
+        char run_log_directory[200];
+        char run_log_data_file[261];
+        char run_log_solution_file[262];
+
+        if (!opt.no_log) {
+            time_t timer;
+            struct tm* tm_info;
+            time(&timer);
+            tm_info = localtime(&timer);
+
+            strftime(time_format, 50, "%Y-%m-%d-%H%M%S", tm_info);
+
+            if (snprintf(run_log_directory, 200, "%s/%s", opt.log_directory, time_format) >= 200) {
+                printf("Unable to allocate run log directory. Terminating.\n");
+                return 1;
+            }
+
+            mkdir(run_log_directory, 0755);
+
+            if (snprintf(run_log_data_file, 261, "%s/%s", run_log_directory, "fitness.dat") >= 261) {
+                printf("Unable to allocate data log path. Terminating.\n");
+                return 1;
+            }
+
+            if (snprintf(run_log_solution_file, 261, "%s/%s", run_log_directory, "solution.dat") >= 262) {
+                printf("Unable to allocate solution log path. Terminating.\n");
+                return 1;
+            }
+
+            PRINT_V("Results will be logged to %s.\n", run_log_data_file);
+
+            f = fopen(run_log_data_file, "a");
         }
 
-        if (snprintf(run_log_solution_file, 261, "%s/%s", run_log_directory, "solution.dat") >= 262) {
-            printf("Unable to allocate solution log path. Terminating.\n");
-            return 1;
+        struct population* children = initialize_population_pool(opt.population_size);
+        struct population* parents = initialize_population_pool(opt.population_size);
+
+        for (int i = 0; i < opt.population_size; i++) {
+            children->individuals[i] = initialize_phenotype(initialize_genotype(&opt));
+            parents->individuals[i] = initialize_phenotype(initialize_genotype(&opt));
+
+            randomize_genotype(children->individuals[i]->genotype, &opt);
         }
 
-        PRINT_V("Results will be logged to %s.\n", run_log_data_file);
+        PRINT_V("A random population has been initialized.\n\n");
 
-        f = fopen(run_log_data_file, "a");
-    }
-
-    struct population* children = initialize_population_pool(opt.population_size);
-    struct population* parents = initialize_population_pool(opt.population_size);
-
-    for (int i = 0; i < opt.population_size; i++) {
-        children->individuals[i] = initialize_phenotype(initialize_genotype(&opt));
-        parents->individuals[i] = initialize_phenotype(initialize_genotype(&opt));
-
-        randomize_genotype(children->individuals[i]->genotype, &opt);
-    }
-
-    PRINT_V("A random population has been initialized.\n\n");
-
-    if (opt.multi_threading) {
-        set_number_of_cores(&opt);
-    }
-
-    signal(SIGINT, sigint_handle);
-
-    struct phenotype* winner = initialize_phenotype(initialize_genotype(&opt));
-    winner->fitness = -1;
-
-    unsigned long long total_cleared_lines = 0;
-
-    for (int i = 0; (i < opt.max_n_generations || opt.max_n_generations == 0) && !user_exit; i++) {
-        PRINT_V("Simulating generation %d.\n", i + 1);
-
-        for (int a = 0; a < opt.population_size; a++) {
-            free_phenotype(parents->individuals[a]);
+        if (opt.multi_threading) {
+            set_number_of_cores(&opt);
         }
 
-        swap_populations(&children, &parents);
+        signal(SIGINT, sigint_handle);
 
-        struct phenotype** parent_pairs = select_parent_pairs(parents, &opt);
+        struct phenotype* winner = initialize_phenotype(initialize_genotype(&opt));
+        winner->fitness = -1;
 
-        for (int a = 0; a < opt.population_size - opt.elitism; a++) {
-            children->individuals[a] = mate_individuals(
-                parent_pairs[a * 2 + 0],
-                parent_pairs[a * 2 + 1],
-                &opt);
-        }
+        unsigned long long total_cleared_lines = 0;
 
-        free(parent_pairs);
+        for (int i = 0; (i < opt.max_n_generations || opt.max_n_generations == 0) && !user_exit; i++) {
+            PRINT_V("Simulating generation %d.\n", i + 1);
 
-        for (int a = 0; a < opt.population_size - opt.elitism; a++) {
-            children->individuals[a]->fitness = average_phenotype_fitness(children->individuals[a], &opt);
-            total_cleared_lines += children->individuals[a]->fitness * opt.n_trials;
-        }
+            for (int a = 0; a < opt.population_size; a++) {
+                free_phenotype(parents->individuals[a]);
+            }
 
-        if (opt.elitism) {
-            qsort(parents->individuals, parents->size, sizeof(struct phenotype*), compare_phenotypes);
+            swap_populations(&children, &parents);
 
-            for (int a = 0; a < opt.elitism; a++) {
-                children->individuals[opt.population_size - opt.elitism + a] = parents->individuals[opt.population_size - opt.elitism + a];
+            struct phenotype** parent_pairs = select_parent_pairs(parents, &opt);
 
-                parents->individuals[opt.population_size - opt.elitism + a] = NULL;
+            for (int a = 0; a < opt.population_size - opt.elitism; a++) {
+                children->individuals[a] = mate_individuals(
+                    parent_pairs[a * 2 + 0],
+                    parent_pairs[a * 2 + 1],
+                    &opt);
+            }
+
+            free(parent_pairs);
+
+            int scheduled_trials_per_individual[opt.population_size - opt.elitism];
+            int finished_trials_per_individual[opt.population_size - opt.elitism];
+            int scheduled_individual_per_worker[n_workers];
+
+            for (int a = 0; a < opt.population_size - opt.elitism; a++) {
+                scheduled_trials_per_individual[a] = 0;
+                finished_trials_per_individual[a] = 0;
+            }
+
+            int tf (int * trials) {
+                for (int a = 0; a < opt.population_size - opt.elitism; a++) {
+                    if (trials[a] < opt.n_trials) {
+                        return 0;
+                    }
+                }
+
+                return 1;
+            }
+
+            int all_trials_scheduled () { return tf(scheduled_trials_per_individual); }
+            int all_trials_finished () { return tf(finished_trials_per_individual); }
+
+            int unfinished_individual () {
+                for (int a = 0; a < opt.population_size - opt.elitism; a++) {
+                    if (scheduled_trials_per_individual[a] < opt.n_trials) {
+                        return a;
+                    }
+                }
+            }
+
+            while (!all_trials_finished()) {
+                int request;
+
+                MPI_Status status;
+
+                MPI_Recv(
+                    &request, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
+                    MPI_COMM_WORLD, &status);
+
+                if (status.MPI_TAG == CALC_PERM_TAG) {
+                    if (all_trials_scheduled()) {
+                        MPI_Send(
+                            &zero, 1, MPI_INT, status.MPI_SOURCE, CALC_PERM_TAG,
+                            MPI_COMM_WORLD);
+                    } else {
+                        int individual = unfinished_individual();
+
+                        scheduled_trials_per_individual[individual]++;
+                        scheduled_individual_per_worker[status.MPI_SOURCE] = individual;
+
+                        MPI_Send(
+                            &one, 1, MPI_INT, status.MPI_SOURCE, CALC_PERM_TAG,
+                            MPI_COMM_WORLD);
+
+                        MPI_Send(
+                            children->individuals[individual]->genotype->feature_weights,
+                            opt.n_features_enabled, MPI_FLOAT, status.MPI_SOURCE,
+                            FEAT_WEIGHTS_TAG, MPI_COMM_WORLD);
+
+                        MPI_Send(
+                            children->individuals[individual]->genotype->feature_enabled,
+                            opt.n_features_enabled, MPI_INT, status.MPI_SOURCE,
+                            FEAT_ENABLED_TAG, MPI_COMM_WORLD);
+                    }
+                } else if (status.MPI_TAG == RESULT_TAG) {
+                    int individual = scheduled_individual_per_worker[status.MPI_SOURCE];
+                    finished_trials_per_individual[individual]++;
+                    children->individuals[individual]->fitness += request;
+                    total_cleared_lines += request;
+
+                    if (finished_trials_per_individual[individual] == opt.n_trials) {
+                        children->individuals[individual]->fitness /= opt.n_trials;
+                    }
+                }
+            }
+
+            if (opt.elitism) {
+                qsort(parents->individuals, parents->size, sizeof(struct phenotype*), compare_phenotypes);
+
+                for (int a = 0; a < opt.elitism; a++) {
+                    children->individuals[opt.population_size - opt.elitism + a] = parents->individuals[opt.population_size - opt.elitism + a];
+
+                    parents->individuals[opt.population_size - opt.elitism + a] = NULL;
+                }
+            }
+
+            if (!opt.no_log) {
+                for (int a = 0; a < opt.population_size; a++) {
+                    fprintf(f, "%d", children->individuals[a]->fitness);
+
+                    if (a == opt.population_size - 1) {
+                        fprintf(f, "\n");
+                    } else {
+                        fprintf(f, "\t");
+                    }
+                }
+
+                fflush(f);
+            }
+
+            struct phenotype generational_winner = {.fitness = -1};
+
+            for (int a = 0; a < opt.population_size; a++) {
+                if (children->individuals[a]->fitness > generational_winner.fitness) {
+                    generational_winner = *children->individuals[a];
+                }
+            }
+
+            if (generational_winner.fitness > winner->fitness) {
+                free_phenotype(winner);
+                winner = copy_phenotype(&generational_winner, &opt);
+            }
+
+            PRINT_V("The best phenotype has a fitness value of %d.\n", generational_winner.fitness);
+
+            if (opt.verbose) {
+                write_phenotype(stdout, &generational_winner, &opt);
             }
         }
 
         if (!opt.no_log) {
-            for (int a = 0; a < opt.population_size; a++) {
-                fprintf(f, "%d", children->individuals[a]->fitness);
+            fclose(f);
 
-                if (a == opt.population_size - 1) {
-                    fprintf(f, "\n");
-                } else {
-                    fprintf(f, "\t");
-                }
-            }
-
-            fflush(f);
+            f = fopen(run_log_solution_file, "a");
+            write_phenotype(f, winner, &opt);
+            fclose(f);
         }
-
-        struct phenotype generational_winner = {.fitness = -1};
-
-        for (int a = 0; a < opt.population_size; a++) {
-            if (children->individuals[a]->fitness > generational_winner.fitness) {
-                generational_winner = *children->individuals[a];
-            }
-        }
-
-        if (generational_winner.fitness > winner->fitness) {
-            free_phenotype(winner);
-            winner = copy_phenotype(&generational_winner, &opt);
-        }
-
-        PRINT_V("The best phenotype has a fitness value of %d.\n", generational_winner.fitness);
 
         if (opt.verbose) {
-            write_phenotype(stdout, &generational_winner, &opt);
+            end = clock();
+
+            double seconds = (double)(end - begin) / CLOCKS_PER_SEC;
+
+            printf("Total cleared lines was %'llu.\n", total_cleared_lines);
+            printf("Execution finished after %.2f seconds.\n", seconds);
+            printf("This amounts to %.2f cleared lines per second.\n", total_cleared_lines / seconds);
+        }
+
+        for (int i = 1; i < n_workers; i++) {
+            MPI_Status status;
+
+            MPI_Recv(
+                &one, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
+                MPI_COMM_WORLD, &status);
+
+            MPI_Send(
+                &one, 1, MPI_INT, status.MPI_SOURCE, SHUTDOWN_TAG,
+                MPI_COMM_WORLD);
+        }
+    } else {
+        while (1) {
+            int result;
+
+            MPI_Status status;
+
+            MPI_Send(
+                &one, 1, MPI_INT, MASTER_RANK, CALC_PERM_TAG, MPI_COMM_WORLD);
+
+            MPI_Recv(
+                &result, 1, MPI_INT, MASTER_RANK, MPI_ANY_TAG,
+                MPI_COMM_WORLD, &status);
+
+            if (status.MPI_TAG == CALC_PERM_TAG) {
+                if (result) {
+                    struct genotype * g = initialize_genotype(&opt);
+
+                    MPI_Recv(
+                        g->feature_weights, opt.n_features_enabled, MPI_FLOAT, MASTER_RANK, FEAT_WEIGHTS_TAG,
+                        MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                    MPI_Recv(
+                        g->feature_enabled, opt.n_features_enabled, MPI_INT, MASTER_RANK, FEAT_ENABLED_TAG,
+                        MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                    struct phenotype * p = initialize_phenotype(g);
+
+                    int fitness = phenotype_fitness(p, &opt);
+
+                    MPI_Send(
+                        &fitness, 1, MPI_INT, MASTER_RANK, RESULT_TAG, MPI_COMM_WORLD);
+
+                    free_phenotype(p);
+                } else {
+                    sleep(1);
+                }
+            } else if (status.MPI_TAG == SHUTDOWN_TAG) {
+                break;
+            }
         }
     }
 
-    if (!opt.no_log) {
-        fclose(f);
-
-        f = fopen(run_log_solution_file, "a");
-        write_phenotype(f, winner, &opt);
-        fclose(f);
-    }
-
-    if (opt.verbose) {
-        end = clock();
-
-        double seconds = (double)(end - begin) / CLOCKS_PER_SEC;
-
-        printf("Total cleared lines was %'llu.\n", total_cleared_lines);
-        printf("Execution finished after %.2f seconds.\n", seconds);
-        printf("This amounts to %.2f cleared lines per second.\n", total_cleared_lines / seconds);
-    }
+    MPI_Finalize();
 }
